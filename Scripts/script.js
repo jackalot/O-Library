a3da3acc95985a7c8ca2062e4cbb65ebdab3a012
jackalotUVM <gonzalezc9999@gmail.com>
2021-09-20 11:07:15 -0400

    can now append to index.html the book with a basic format

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,4 +1,8 @@
-let myLibrary = [];
+let myLibrary = [{
+    title: "this is a title",
+    description: "this is a description",
+    author: "this is a author",
+}];
 
 function Book(title, description, author) {
     this.title = title;
@@ -13,9 +17,26 @@ function addBookToLibrary (titleI, descI, authI) {
     newBook.author = authI;
     myLibrary.push(newBook);
 }
+const body = document.querySelector("body");
+displayBook();
 function displayBook () {
-    for (let index = 0; index < array.length; index++) {
-        const element = array[index];
-        
+    for (let index = 0; index < myLibrary.length; index++) {
+        const bookObj = myLibrary[index];
+        const div = document.createElement("div");
+        div.classList.add("Book");
+        div.id = `${bookObj.title}`;
+        body.append(div);
+        const h2 = document.createElement("h2");
+        h2.classList.add("title");
+        h2.textContent = bookObj.title;
+        div.append(h2);
+        const h3 = document.createElement("h3");
+        h3.classList.add("author");
+        h3.textContent = bookObj.author;
+        div.append(h3);
+        const p = document.createElement("p");
+        p.classList.add("description");
+        p.textContent = bookObj.description;
+        div.append(p);
     }
 }
