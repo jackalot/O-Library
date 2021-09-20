5eb0855f26a4ede18fac680eba9c47a8a816cfdb
jackalotUVM <gonzalezc9999@gmail.com>
2021-09-20 10:24:20 -0400

    now can create  a book object

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,1 +1,22 @@
-console.log("hello world");
+let myLibrary = [];
+
+function Book(title, description, author) {
+    this.title = title;
+    this.description = description;
+    this.author = author;
+}
+//I stands for input
+function addBookToLibrary (titleI, descI, authI) {
+    let newBook = Object.create(Book);
+    newBook.title = titleI;
+    newBook.description = descI;
+    newBook.author = authI;
+    myLibrary.push(newBook);
+}
+function displayBook () {
+    
+    for (let index = 0; index < myLibrary.length; index++) {
+        const element = array[index];
+        
+    }
+}
