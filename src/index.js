e83dbe4f00488ab8db137505a41a4dd6e3c00038
Jackalot <gonzalezc9999@gmail.com>
2022-06-07 15:34:01 -0400

    added a comment for where we create the author elements

--- src/index.js
+++ src/index.js
@@ -90,12 +90,14 @@ function displayBook() {
     div.classList.add("Book");
     div.id = `${bookObj.title}`;
     bookDiv.append(div);
-    //  create the title of the book,
+    //  create the title element of the book,
     //  in this case, an h2
     const h2 = document.createElement("h2");
     h2.classList.add("title");
     h2.textContent = bookObj.title;
     div.append(h2);
+    //  create the author element of the book,
+    //  in this case, an h3
     const h3 = document.createElement("h3");
     h3.classList.add("author");
     h3.textContent = bookObj.author;
