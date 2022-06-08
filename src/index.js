7f6275c3638dd647224c9355ab2fb41f01e4df57
Jackalot <gonzalezc9999@gmail.com>
2022-06-08 09:17:05 -0400

    createTitle now returns it's h2, createAuthor now makes an h3 element similar to displayBook

--- src/index.js
+++ src/index.js
@@ -159,9 +159,16 @@ function listAllBooks() {
     const h2 = document.createElement("h2");
     h2.classList.add("title");
     h2.textContent = bookObj.title;
-    div.append(h2);
+    return h2;
   }
-  function createAuthor() {}
+  function createAuthor(bookObj) {
+    //  create the author element of the book,
+    //  in this case, an h3
+    const h3 = document.createElement("h3");
+    h3.classList.add("author");
+    h3.textContent = bookObj.author;
+    return h3;
+  }
   function createDescription() {}
   function createReadButton() {}
   for (let index = 0; index < myLibrary.length; index++) {}
